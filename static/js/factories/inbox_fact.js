923bc7bac54824f790b88dac1117413c989d0b68
Nabeel Virani <nabeelvirani@gmail.com>
2016-05-09 16:58:17 -0500

    added inbox factory and json file

---
+++ static/js/factories/inbox_fact.js
@@ -0,0 +1,18 @@
+var app = angular.module("app");
+
+
+app.factory("inboxfact", ['$http', function($http){
+	var exports = {};
+
+
+	exports.getMessages = function(){
+		return $http.get('json/emails.json')
+			.error(function(data){
+				console.log("Error!!!", data)
+			});
+
+	};
+
+	return exports
+
+}]);
